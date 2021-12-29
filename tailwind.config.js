0a18a3cfbceb438548a10fa38e0a5829f1765637
erica smith <ericasmith2@gmail.com>
2021-12-29 18:46:13 -0500

    troubleshooting postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./public/**/*.html", "./src/**/*.js"],
+  purge: ["./public/**/*.html", "./src/**/*.js", "./src/**/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
